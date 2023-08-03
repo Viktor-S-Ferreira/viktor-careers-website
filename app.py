94e0576c05550bb569e14d731a08553c36cd037a
Viktor-S-Ferreira <141160595+Viktor-S-Ferreira@users.noreply.github.com>
2023-08-03 19:00:03 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -3,11 +3,27 @@ import this
 
 app = Flask(__name__)
 
+PROJECTS = [
+  
+  {
+    'id': 1,
+    'title': 'project1'
+  },
+  {
+    'id': 2,
+    'title': 'project2'
+  },
+  {
+    'id': 3,
+    'title': 'project3'
+  }
+
+]
 
 @app.route("/")
 
 def hello():
-  return render_template('home.html')
+  return render_template('home.html', projects = PROJECTS, admName = 'Viktor')
 
 if(__name__ == "__main__"):
   app.run(host = "0.0.0.0", debug = True)
